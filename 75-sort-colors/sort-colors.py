3d6d64733bfd63f36a1bca44ce56483f00855f5a
CEDRIC Ntwari <ntwaricedrick001@gmail.com>
2026-01-18 19:15:23 +0200

    Time: 3 ms (17.98%) | Memory: 12.4 MB (85.73%) - LeetSync

---
+++ 75-sort-colors/sort-colors.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def sortColors(self, nums):
+        n = len(nums)
+        for i in range(n-1):
+            for j in range(i+1, n):
+                if nums[i] > nums[j]:
+                    nums[i], nums[j] = nums[j], nums[i]
